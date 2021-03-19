19950e73aff682971fda197f3b20b8d970c28564
rockerishFox <delia.csiki.dely@gmail.com>
2021-03-20 00:42:06 +0200

    Style RoomCard Close button

--- src/Rooms/RoomCard/RoomCard.js
+++ src/Rooms/RoomCard/RoomCard.js
@@ -2,10 +2,20 @@ import React, { useState } from "react";
 import AddIcon from "@material-ui/icons/Add";
 import cssClasses from "./RoomCard.module.css";
 import { Button } from "@material-ui/core";
+import WhiteCloseBtn from '../../resources/icons/whiteX.svg';
 
 const RoomCard = ({ name, type = null, clickHandler, deleteHandler }) => {
   const classes = [cssClasses.RoomCard];
   const [isPlusCard, setIsPlusCard] = useState(type === "ADD_ROOM");
+  const [toggleCloseBtn, setToggleCloseBtn] = useState(false);
+
+  const handleMouseEnter = () => {
+    setToggleCloseBtn(true);
+  }
+
+  const handleMouseLeave = () => {
+    setToggleCloseBtn(false);
+  }
 
   var content = <p>{name}</p>;
   if (isPlusCard) {
@@ -16,17 +26,17 @@ const RoomCard = ({ name, type = null, clickHandler, deleteHandler }) => {
   }
 
   var deleteBtn = (
-    <div className={cssClasses.DeleteBtn} onClick={deleteHandler}>
-      -
+    <div className={cssClasses.DeleteBtn} onClick={deleteHandler} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <img className={cssClasses.DeleteIcon} src={WhiteCloseBtn} alt="Close" />
     </div>
   );
 
   return (
     <div className={cssClasses.CardContainer}>
-      <div className={classes.join(" ")} onClick={clickHandler}>
+      <div className={classes.join(" ")} onClick={clickHandler} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
         {content}
       </div>
-      {!isPlusCard ? deleteBtn : null}
+      {!isPlusCard && toggleCloseBtn ? deleteBtn : null}
     </div>
   );
 };
