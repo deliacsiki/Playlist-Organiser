252bb095b23a316584d4adf60cca9820d272c333
rockerishFox <delia.csiki.dely@gmail.com>
2021-04-11 15:12:38 +0300

    refactor Backdrop
    - make close button stick to top right

--- src/UI/Backdrop/Backdrop.js
+++ src/UI/Backdrop/Backdrop.js
@@ -1,13 +1,37 @@
-import { Button } from "@material-ui/core";
+import { Button, makeStyles } from "@material-ui/core";
 import cssClasses from "./Backdrop.module.css";
 
-const Backdrop = ({ children, toggleBackdrop }) => (
-  <div className={cssClasses.BackdropBackground}>
-    <div className={cssClasses.BackdropContent}>
-      <Button onClick={toggleBackdrop}>X</Button>
-      {children}
+const useStyles = makeStyles({
+  backdropContent: {
+    backgroundColor: "white",
+    borderRadius: "10px",
+    minWidth: "50%",
+    maxWidth: "80%",
+    height: "80%",
+    margin: "2rem auto",
+    padding: "1rem",
+    position: "relative",
+    overflow: "hidden",
+  },
+  closeBtn: {
+    position: "absolute",
+    right: 0,
+    top: 0,
+  },
+});
+
+const Backdrop = ({ children, toggleBackdrop }) => {
+  const classes = useStyles();
+  return (
+    <div className={cssClasses.BackdropBackground}>
+      <div className={classes.backdropContent}>
+        <Button className={classes.closeBtn} onClick={toggleBackdrop}>
+          X
+        </Button>
+        {children}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Backdrop;
